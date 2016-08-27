876052b8ec20b80c01808cb1b06fa698450d766b
regiondavid <uestczjw@gmail.com>
2016-08-27 23:13:20 +0800

    change the share

--- app2/js/index1.js
+++ app2/js/index1.js
@@ -270,7 +270,11 @@ $(function($){
 
  	wx.config({
 	    debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-    	jsApiList: ['onMenuShareTimeline','onMenuShareAppMessage','onMenuShareQQ','onMenuShareWeibo','onMenuShareQZone'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+	    appId: 'wxe9121b636db7c039',
+	    timestamp: "1472309147", 
+	    nonceStr: "weilan", 
+	    signature: "2901893c6e409f99ed5133b688bd50c391b3661f",
+	   	jsApiList: ['onMenuShareTimeline','onMenuShareAppMessage','onMenuShareQQ','onMenuShareWeibo','onMenuShareQZone'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
 	});
 
  	wx.ready(function(){
@@ -342,6 +346,9 @@ $(function($){
 		        // 用户取消分享后执行的回调函数
 		    }
 		});
+	});
+	wx.error(function(res) {
+		alert("hahahha");
 	});
 
 })
